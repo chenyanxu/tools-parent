8b4398b841b58923d10edf6c975905a4600c620f
chenyanxu <yanxu2016>
2016-09-22 18:11:07 +0800

    command for init the sequence table after init-db command finish

---
+++ tools-karaf-command/src/main/java/com/kalix/tools/command/InitSequenceCommand.java
@@ -0,0 +1,74 @@
+package com.kalix.tools.command;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+import javax.sql.DataSource;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by sunlf on 2015/12/4.
+ * 运行特定的sql脚本，通过参数指定脚本名称
+ */
+@Command(scope = "kalix", name = "init-sequence", description = "init openjpaseq table")
+@Service
+public class InitSequenceCommand implements Action {
+    @Override
+    public Object execute() throws Exception {
+        DataSource dataSource = Util.getKalixDataSource();
+        Connection conn = dataSource.getConnection();
+        DatabaseMetaData dbMetaData = conn.getMetaData();
+        String[] types = {"TABLE"};
+        ResultSet tabs = dbMetaData.getTables(null, null, null, types);
+        ArrayList<String> tableNames=new ArrayList<>();
+        ArrayList<String> targetTableNames=new ArrayList<>();
+        ArrayList<Integer> ids=new ArrayList<>();
+
+        //get all table name
+        while (tabs.next()) {
+            tableNames.add(tabs.getString("TABLE_NAME"));
+        }
+
+        tabs.close();
+
+        ResultSet columns=null;
+
+        //get the table that contains the 'id' column
+        for (String tableName:tableNames) {
+            columns = dbMetaData.getColumns(null, "%", tableName, "id");
+
+            while (columns.next()) {
+                targetTableNames.add(tableName);
+            }
+
+            columns.close();
+        }
+
+        Statement statement = conn.createStatement();
+        ResultSet rs=null;
+        //get the max id for each table
+        for (String tableName:targetTableNames) {
+            rs=statement.executeQuery("SELECT MAX(id) FROM "+tableName);
+            rs.next();
+            ids.add(rs.getInt(1));
+            rs.close();
+        }
+
+        Object[] idsSort= ids.toArray();
+        //sort the id list,the last one will at the end.
+        Arrays.sort(idsSort);
+        //update the sequence table
+        statement.execute("UPDATE openjpaseq set sequence_value="+idsSort[ids.size()-1].toString());
+
+        return null;
+    }
+
+
+}
