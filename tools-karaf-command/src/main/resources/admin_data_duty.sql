12c88122f91ce1bd9d88581d52465735327e47b0
minikiller <minikiller@qq.com>
2017-10-13 13:58:43 +0800

    add 2 dataauth sql

--- tools-karaf-command/src/main/resources/admin_data_duty.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM public.sys_data_duty;
-INSERT INTO public.sys_data_duty (id, createby, createbyid, creationdate, updateby, updatebyid, updatedate, comment, name, version_) VALUES (1, '管理员', null, '2017-10-12 15:00:00.000000', '管理员', null, '2017-10-12 15:00:00.000000', '仅本人数据', '仅本人数据', 1);
-INSERT INTO public.sys_data_duty (id, createby, createbyid, creationdate, updateby, updatebyid, updatedate, comment, name, version_) VALUES (2, '管理员', null, '2017-10-12 15:00:00.000000', '管理员', null, '2017-10-12 15:00:00.000000', '所有数据', '所有数据', 1);
-INSERT INTO public.sys_data_duty (id, createby, createbyid, creationdate, updateby, updatebyid, updatedate, comment, name, version_) VALUES (3, '管理员', null, '2017-10-12 15:00:00.000000', '管理员', null, '2017-10-12 15:00:00.000000', '所在组织机构及以下数据', '所在组织机构及以下数据', 1);
-INSERT INTO public.sys_data_duty (id, createby, createbyid, creationdate, updateby, updatebyid, updatedate, comment, name, version_) VALUES (4, '管理员', null, '2017-10-12 15:00:00.000000', '管理员', null, '2017-10-12 15:00:00.000000', '所在组织机构数据', '所在组织机构数据', 1);
