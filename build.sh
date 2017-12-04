3981c74821aac95e46bb2e04bdaa06e516859f72
minikiller <minikiller@qq.com>
2017-12-04 20:55:46 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,29 @@
 cd kalix-parent && git pull origin master && mvn clean install
-&& cd ..
+cd ..
 
-&& cd framework-parent && git pull origin master && call mvn -DskipTests=true clean install
-&& cd ..
+cd framework-parent && git pull origin master && mvn -DskipTests=true clean install
+cd ..
 
-&& cd admin-parent & git pull origin master & call mvn  -DskipTests=true clean install
-&& cd ..
+cd admin-parent && git pull origin master && mvn  -DskipTests=true clean install
+cd ..
 
-&& cd middleware-parent & git pull origin master & call mvn  -DskipTests=true clean install
-&& cd ..
+cd middleware-parent && git pull origin master && mvn  -DskipTests=true clean install
+cd ..
 
-&& cd oa-parent & git pull origin master & call mvn clean install
-&& cd ..
+cd oa-parent && git pull origin master && mvn clean install
+cd ..
 
-&& cd common-parent & git pull origin master & call mvn clean install
-&& cd ..
+cd common-parent && git pull origin master && mvn clean install
+cd ..
 
-cd schedule-parent & git pull origin master & call mvn clean install
-&& cd ..
+cd schedule-parent && git pull origin master && mvn clean install
+cd ..
 
-cd tools-parent & git pull origin master & call mvn clean install
-&& cd ..
+cd tools-parent && git pull origin master && mvn clean install
+cd ..
 
-cd research-parent & git pull origin master & call mvn clean install
-&& cd ..
+research-parent && git pull origin master && mvn clean install
+cd ..
+
+research-parent && git pull origin master && mvn clean install
+cd ..
