71f4b159d3db1c8bcfa9a01397f29abefd8088b1
houqingjie <150091225@qq.com>
2018-03-21 09:44:38 +0800

    新增艺术中心角色授权sql脚本，语句结束后缺少";"

--- tools-karaf-command/src/main/resources/art_test.sql
+++ tools-karaf-command/src/main/resources/art_test.sql
@@ -46,11 +46,11 @@ select setval('sys_role_application_id_seq', max(id)) from sys_role_application;
 INSERT INTO "public"."sys_role_application" ("id", "createby", "createbyid", "creationdate", "updateby", "updatebyid", "updatedate", "applicationid", "roleid", "version_")
   SELECT nextval('sys_role_application_id_seq') AS id, 'admin' AS createby, NULL AS createbyid, NULL AS creationdate,
        'admin' AS updateby, NULL AS updatebyid, '2018-03-16 14:26:10.215' AS updatedate,
-       id AS applicationid, '702' AS roleid, '1' AS version_ FROM sys_application WHERE NAME = '艺术中心'
+       id AS applicationid, '702' AS roleid, '1' AS version_ FROM sys_application WHERE NAME = '艺术中心';
 INSERT INTO "public"."sys_role_application" ("id", "createby", "createbyid", "creationdate", "updateby", "updatebyid", "updatedate", "applicationid", "roleid", "version_")
   SELECT nextval('sys_role_application_id_seq') AS id, 'admin' AS createby, NULL AS createbyid, NULL AS creationdate,
        'admin' AS updateby, NULL AS updatebyid, '2018-03-16 14:26:30.22' AS updatedate,
-       id AS applicationid, '703' AS roleid, '1' AS version_ FROM sys_application WHERE NAME = '艺术中心'
+       id AS applicationid, '703' AS roleid, '1' AS version_ FROM sys_application WHERE NAME = '艺术中心';
 
 --add 为艺术中心角色function授权
 DROP SEQUENCE IF EXISTS "public"."sys_role_function_id_seq";
@@ -83,7 +83,7 @@ UNION
 select t.id from sys_function t where t.parentid in (
 	select fun.id from sys_function fun, sys_application app WHERE fun.applicationid=app.id and app.name = '艺术中心' and fun.datapermissionkey='company')
 
-) s order by s.functionid) a
+) s order by s.functionid) a;
 
 INSERT INTO "public"."sys_role_function" ("id", "createby", "createbyid", "creationdate", "updateby", "updatebyid", "updatedate", "functionid", "roleid", "version_")
 SELECT nextval('sys_role_function_id_seq') AS id, 'admin' AS createby, NULL AS createbyid, NULL AS creationdate,
@@ -110,5 +110,5 @@ UNION
 select t.id from sys_function t where t.parentid in (
 	select fun.id from sys_function fun, sys_application app WHERE fun.applicationid=app.id and app.name = '艺术中心' and fun.datapermissionkey='artstudent')
 
-) s order by s.functionid) a
+) s order by s.functionid) a;
 
