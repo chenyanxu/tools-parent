2c144b4f4d70b01c8ad0350c331223fe7add127a
minikiller <minikiller@qq.com>
2018-12-10 19:45:25 +0800

    add permit sync function

--- install.bat
+++ install.bat
@@ -30,11 +30,6 @@ cd common-parent
 call mvn clean install
 cd ..
 
-git clone https://github.com/chenyanxu/schedule-parent.git
-cd schedule-parent
-call mvn clean install
-cd ..
-
 git clone https://github.com/chenyanxu/tools-parent.git
 cd tools-parent
 call mvn clean install
