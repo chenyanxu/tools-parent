ad907b37ced161f19eeb374ade5006ee6236138f
minikiller <minikiller@qq.com>
2019-04-23 16:09:26 +0800

    add vuejs project to tools

---
+++ tools-karaf-vue/src/main/java/com/kalix/trying/event/vue/internal/InitActivator.java
@@ -0,0 +1,24 @@
+package com.kalix.trying.event.vue.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+/**
+ * Created by sunlf on 18-12-16.
+ */
+public class InitActivator implements BundleActivator {
+
+  @Override
+  public void start(BundleContext bundleContext) throws Exception {
+    System.out.println("start vue project!");
+  }
+
+  @Override
+  public void stop(BundleContext bundleContext) throws Exception {
+    System.out.println("stop vue project!");
+  }
+
+
+}
+
